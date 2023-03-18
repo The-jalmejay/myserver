760b294ce3999bc717cefd30abfaa5185e6d3553
The-Jalmejay <jalmejaykumarshaw10@gmail.com>
2023-03-18 12:12:22 +0530

    Initial Commit

--- myserver.js
+++ myserver.js
@@ -1,5 +1,6 @@
 express = require("express");
 let app = express();
+let cors = require("cors");
 app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,6 +18,13 @@ var port = process.env.port || 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port} !`));
 let axios = require("axios");
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.post("/myserver", async function (req, res) {
   let rep = req.body;
   // console.log(req);
@@ -29,7 +37,7 @@ app.post("/myserver", async function (req, res) {
     } catch (error) {
       if (error.response) {
         let { status, statusText } = error.response;
-        console.log("error",status, statusText);
+        console.log("error", status, statusText);
         res.status(status).send(statusText);
       } else res.status(404).send(error);
     }
@@ -38,7 +46,7 @@ app.post("/myserver", async function (req, res) {
       let body = data;
       console.log(body);
       let response = await axios.post(fetchUrl, body);
-      console.log("resp",response.data);
+      console.log("resp", response.data);
       res.send(response.data);
     } catch (error) {
       if (error.response) {
